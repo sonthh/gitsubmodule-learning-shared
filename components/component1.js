54a6584d6fd4dbdfbb76ab5a3b3fcdba20b9beb4
sonthh <tranhuuhongson@gmail.com>
2024-04-23 13:45:12 +0700

    component read env variable

--- components/component1.js
+++ components/component1.js
@@ -3,6 +3,7 @@ const clsx = require("clsx");
 const component1 = {
   name: "component1 Updated",
   classNames: "Shared use clsx" + clsx("foo", true && "bar", "baz"),
+  envVariable: "Shared use .env author=" + process.env.AUTHOR,
 };
 
 module.exports = {
